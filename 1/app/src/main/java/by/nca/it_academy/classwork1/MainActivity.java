38028a9a18377ee7ec814d80223a6058e1afb899
Antanina Miruk <sidtonya@yandex.ru>
2018-02-08 22:19:14 +0300

    trany

--- 1/app/src/main/java/by/nca/it_academy/classwork1/MainActivity.java
+++ 1/app/src/main/java/by/nca/it_academy/classwork1/MainActivity.java
@@ -1,8 +1,26 @@
 package by.nca.it_academy.classwork1;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.widget.Button;
+import android.widget.TextView;
+
+import by.nca.it_academy.R;
+
 
 public class MainActivity extends Activity {
+    private Button button1;
+    private TextView TextView1;
+    private TextView TextView2;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_classwork1);
+    }
+
+
 
 
 
