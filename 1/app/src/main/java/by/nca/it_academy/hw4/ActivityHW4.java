616d3359574fac0b34a3fa94dabde6cc7f680cb6
Miruk <miruk@nca.by>
2018-02-19 17:49:07 +0300

    HW1

--- 1/app/src/main/java/by/nca/it_academy/hw4/ActivityHW4.java
+++ 1/app/src/main/java/by/nca/it_academy/hw4/ActivityHW4.java
@@ -1,9 +1,12 @@
 package by.nca.it_academy.hw4;
 
 import android.app.Activity;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 import by.nca.it_academy.R;
 
@@ -12,10 +15,22 @@ import by.nca.it_academy.R;
  */
 
 public class ActivityHW4 extends Activity {
+    private ImageView sovaImage;
+    AnimationDrawable animationDrawable;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         setContentView(R.layout.activity_homework4);
         super.onCreate(savedInstanceState);
+
+        sovaImage = findViewById(R.id.sovaImage);
+        /*sovaImage.setOnClickListener((View.OnClickListener) this);
+        sovaImage.setBackgroundResource(R.drawable.sova_animation);
+        animationDrawable = (AnimationDrawable)sovaImage.getBackground();
+        animationDrawable.start();*/
+
+
+
+
     }
 }
