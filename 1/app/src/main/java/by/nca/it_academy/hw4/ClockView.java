2cd0e228292fb44ed3703ec125174c916b1d83cb
Antanina Miruk <sidtonya@yandex.ru>
2018-02-21 22:10:33 +0300

    HW4

--- 1/app/src/main/java/by/nca/it_academy/hw4/ClockView.java
+++ 1/app/src/main/java/by/nca/it_academy/hw4/ClockView.java
@@ -7,6 +7,7 @@ import android.graphics.Paint;
 import android.graphics.RectF;
 import android.graphics.drawable.shapes.Shape;
 import android.os.Build;
+import android.os.CountDownTimer;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
@@ -16,6 +17,7 @@ import android.view.View;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.Timer;
 
 import static java.lang.Math.round;
 
@@ -31,6 +33,7 @@ public class ClockView extends View {
     private final Integer HOUR = 12;
     private Calendar calendar;
     private Date date;
+    private Timer timer;
 
     public ClockView(Context context) {
         super(context);
@@ -62,6 +65,8 @@ public class ClockView extends View {
         date = new Date();
         calendar = GregorianCalendar.getInstance();
         calendar.setTime(date);
+        timer = new Timer();
+
     }
 
     @Override
@@ -77,10 +82,12 @@ public class ClockView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        float width = canvas.getWidth();
+        float height = canvas.getHeight();
 
-        canvas.drawCircle(cx, cy, radius, paint);
+        canvas.drawCircle(width / 2f, height / 4f, radius, paint);
         paint.setColor(Color.parseColor("#111111"));
-        canvas.drawCircle(cx, cy, 5, paint);
+        canvas.drawCircle(width / 2f, height / 4f, 5, paint);
         canvas.save();
         int rotateCanvas = 30;
         paint.setTextSize(radius / 6);
@@ -89,18 +96,18 @@ public class ClockView extends View {
             canvas.rotate(rotateCanvas, cx, cy);
         }
         ;
-        canvas.drawText("6", cy + Math.round(radius * 0.9), cy + radius, paint);
-        canvas.drawText("9", cy, cy + Math.round(radius * 0.9), paint);
-        canvas.drawText("3", cy, cy - Math.round(radius * 0.9), paint);
-        canvas.drawText("12", cy - Math.round(radius * 0.9), cy + radius, paint);
+        canvas.drawText("6", width / 2f, height / 4f+Math.round(radius * 0.8), paint);
+        canvas.drawText("9", width / 2f+Math.round(radius * 0.8), height / 4f, paint);
+        canvas.drawText("3", width / 2f-Math.round(radius * 0.8), height / 4f, paint);
+        canvas.drawText("12", width / 2f, height / 4f-Math.round(radius * 0.8), paint);
+        paint.setStrokeWidth(5f);
+        canvas.save();
         int hour = calendar.get(Calendar.HOUR);
         int minute = calendar.get(Calendar.MINUTE);
-        paint.setStrokeWidth(20f);
-        canvas.save();
         canvas.rotate(minute * 6 - 180, cx, cy);
         canvas.drawLine(cx, cy, cx, cy + radius - cy * 0.1f, paint);
         canvas.restore();
-        paint.setStrokeWidth(30f);
+        paint.setStrokeWidth(10f);
         canvas.save();
         canvas.rotate(hour * 30 - 180 + minute * 0.5f, cx, cy);
         canvas.drawLine(cx, cy, cx, cy + radius - cy * 0.2f, paint);
