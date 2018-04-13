4f80ff21a023c2b5249504e59a436d8804400bc0
Antanina Miruk <sidtonya@yandex.ru>
2018-04-13 21:09:14 +0300

    CW20

--- 1/presentation/build/generated/source/buildConfig/androidTest/dev/debug/by/nca/it_academy/test/BuildConfig.java
+++ 1/presentation/build/generated/source/buildConfig/androidTest/dev/debug/by/nca/it_academy/test/BuildConfig.java
@@ -8,7 +8,7 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "by.nca.it_academy.test";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "dev";
-  public static final int VERSION_CODE = 1;
+  public static final int VERSION_CODE = 2;
   public static final String VERSION_NAME = "1.0";
   // Fields from product flavor: dev
   public static final String API_URL = "http://test";
--- 1/presentation/build/generated/source/dataBinding/dev/debug/android/databinding/layouts/DataBindingInfo.java
+++ 1/presentation/build/generated/source/dataBinding/dev/debug/android/databinding/layouts/DataBindingInfo.java
@@ -2,5 +2,5 @@ package android.databinding.layouts;
 
 import android.databinding.BindingBuildInfo;
 
-@BindingBuildInfo(buildId="5e8e4229-3eba-4866-8e33-094afceb14ea")
+@BindingBuildInfo(buildId="9caf0404-0b9a-42df-a0a6-fbb7f521ac00")
 public class DataBindingInfo {}
